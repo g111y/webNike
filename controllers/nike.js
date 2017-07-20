e32af12f1489a99ec2b9af80418edf470c59f567
unknown <gaoyu@LGPARKSON.com>
2017-07-20 16:53:53 +0800

    1

--- controllers/nike.js
+++ controllers/nike.js
@@ -6,21 +6,25 @@ class nike {
     // constructor() {
     //     console.log("this is nike");
     // }
-    async gdno(ctx, next) {//根据货号查询单品信息
+    async gdno(ctx, next) { //根据货号查询单品信息
         await next();
         let data = {};
         let gdno = ctx.request.body.gdno;
         let valueLocal = await query.listLocal(gdno); //先查本地数据库是否有资料
         console.log(`本地查询到${valueLocal.length}条数据!`);
-
-        if (valueLocal.length != 0) {//如果本地数据库有信息，取本地信息
-            valueLocal[0].bar = await query.listBarLocal(valueLocal[0].itemNo);
-            // console.log(valueLocal.bar);
-            data.success = true;
-            data.item = valueLocal[0];
-            ctx.body = data;
-            return;
+        try {
+            if (valueLocal.length != 0) { //如果本地数据库有信息，取本地信息
+                valueLocal[0].bar = await query.listBarLocal(valueLocal[0].itemNo);
+                // console.log(valueLocal.bar);
+                data.success = true;
+                data.item = valueLocal[0];
+                ctx.body = data;
+                return;
+            }
+        }catch(err){
+            
         }
+
 
         //取NK系统的信息
         let stat = await login();
